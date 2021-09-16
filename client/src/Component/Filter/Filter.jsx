9bd49ffbe56b3f41553a32c7a694382b2fea5005
Denis Shulgin <d.a.shulgin2019@ya.ru>
2021-09-16 13:05:16 +0300

    данные с сервера/Пагинатор/Фильтр

--- client/src/Component/Filter/Filter.jsx
+++ client/src/Component/Filter/Filter.jsx
@@ -1,55 +1,11 @@
-import React, { useEffect, useState }  from 'react';
-import Paginatior from '../Paginatior/Paginatior';
-import Table from '../Table/Table';
+import React from 'react';
 import style from './filter.module.css';
 
 
-const Filter = ({ products }) => {
+const Filter = ({value1, SetValueList1, value2, SetValueList2, text, SetText }) => {
 
-    let limitCountPage = 20;  //кол-во на одной страниц
-
-
-    let [totalItemCount, setTotalItemCount] = useState(0);
-    let [pagesCount, setPagesCount] = useState(0);
-    let [currentPage, setCurrentPage] = useState(1);
-
-    useEffect(()=>{
-        if(!products){
-            return
-        }
-        setTotalItemCount(products.length); 
-        const getPagesCount = Math.ceil(totalItemCount / limitCountPage);
-        setPagesCount(getPagesCount);
-    })
-
-
-    let pages = [];
-    for (let i = 1; i <= pagesCount; i++) {
-        pages.push(i);
-    }
-
-
-    let lastBlockRow  = currentPage * limitCountPage; 
-    let firstBlockRow = (lastBlockRow - limitCountPage); 
-    let currnetBlockRows = products.slice(firstBlockRow, lastBlockRow )
-
-
-
-    const onNextClick = ()=>{
-        if(currentPage > pagesCount -1){
-            return
-        }
-        setCurrentPage(currentPage + 1);
-
-    }
-    const onPreviosClick = ()=> {
-        if(currentPage < 2){
-            return
-        }
-        setCurrentPage(currentPage -1);
-    }
     const parametrList1 = [
-        { value: "name", columnsName: "Название" },
+        { value: "names", columnsName: "Название" },
         { value: "amount", columnsName: "Количество" },
         { value: "distance", columnsName: "Расстояние" }
     ]
@@ -60,32 +16,6 @@ const Filter = ({ products }) => {
         { value: "contains", columnsName: "Cодержит" }
     ]
 
-    let [value1, SetValueList1] = useState('name');
-    let [value2, SetValueList2] = useState('equals');
-    let [text, SetText] = useState('');
-
-
-   
-    const getFiltredData = () => {
-        if (!text) {          
-            return currnetBlockRows
-        }
-        else {
-            const temp1 = value1;
-            const temp2 = text;
-            switch(value2){
-                        case "contains": return products.filter( el => { 
-                            return el[value1].toLowerCase().includes(text.toLowerCase()); 
-
-                        });
-                        case "more": return products.filter( el => el[value1] > text);   
-                        case "less": return products.filter( el => el[value1] < text);  
-                        case "equals": return products.filter( el => el[value1] == text);                              
-                    }                      
-                }      
-        }
-    
- const filtredProducts = getFiltredData();
 
     const SelectedList = ({ parametrList, nameList, value, SetValueList }) => {
 
@@ -106,14 +36,6 @@ const Filter = ({ products }) => {
             <SelectedList nameList="Условие" parametrList={parametrList2} value={value2} SetValueList={SetValueList2} />
             <span><input type="text" value={text} onChange={(e) => SetText(e.target.value)} /></span>
         </div>
-        <Table products={filtredProducts} />
-        {!text ? <Paginatior 
-        pages={pages} 
-        setCurrentPage = {setCurrentPage} 
-        currentPage = {currentPage}
-        onNextClick = {onNextClick}
-        onPreviosClick = {onPreviosClick}
-        /> :null}
     </div>
 }
 
--- client/src/Component/Product/Product.jsx
+++ client/src/Component/Product/Product.jsx
@@ -1,11 +1,91 @@
-import React from "react";
+import React, { useEffect, useState }  from 'react';
+import Filter from '../Filter/Filter';
 import Table from '../Table/Table';
+import Paginatior from '../Paginatior/Paginatior';
 
 
-const Product = ({products}) =>{
+const Product = ({products}) => {
+
+    let [value1, SetValueList1] = useState('names');
+    let [value2, SetValueList2] = useState('equals');
+    let [text, SetText] = useState('');
+   
+    const getFiltredData = () => {
+        if (!text) {          
+            return products
+        }
+        else {
+            switch(value2){
+                        case "contains": return products.filter( el => { 
+                            return el[value1].toString().toLowerCase().includes(text.toLowerCase()); 
+
+                        });
+                        case "more": return products.filter( el => el[value1] > text);   
+                        case "less": return products.filter( el => el[value1] < text);  
+                        case "equals": return products.filter( el => el[value1] == text);                              
+                    }                      
+                }      
+        }
+    
+ const filtredProducts = getFiltredData();
+
+    let limitCountPage = 5;  //кол-во на одной страниц
+
+
+    let [totalItemCount, setTotalItemCount] = useState(0);
+    let [pagesCount, setPagesCount] = useState(0);
+    let [currentPage, setCurrentPage] = useState(1);
+
+    useEffect(()=>{
+        if(!products){
+            return
+        }
+        setTotalItemCount(filtredProducts.length); 
+        const getPagesCount = Math.ceil(totalItemCount / limitCountPage);
+        setPagesCount(getPagesCount);
+    })
+
+
+    let pages = [];
+    for (let i = 1; i <= pagesCount; i++) {
+        pages.push(i);
+    }
+
+
+    let lastBlockRow  = currentPage * limitCountPage; 
+    let firstBlockRow = (lastBlockRow - limitCountPage); 
+    let currnetBlockRows = filtredProducts.slice(firstBlockRow, lastBlockRow )
+
+
+    const onNextClick = ()=>{
+        if(currentPage > pagesCount -1){
+            return
+        }
+        setCurrentPage(currentPage + 1);
+
+    }
+    const onPreviosClick = ()=> {
+        if(currentPage < 2){
+            return
+        }
+        setCurrentPage(currentPage -1);
+    } 
 
 return<div>
-    <Table products = {products}/>
+    <Filter 
+        value1 = {value1}
+        SetValueList1 = {SetValueList1}
+        value2 = {value2}
+        SetValueList2 = {SetValueList2}
+        text = {text}
+        SetText = {SetText} />
+    <Table  products = {currnetBlockRows} />
+    <Paginatior  
+        pages={pages} 
+        setCurrentPage = {setCurrentPage} 
+        currentPage = {currentPage}
+        onNextClick = {onNextClick}
+        onPreviosClick = {onPreviosClick} /> 
     </div>
 }
 
